486cc982cf4e31976f000dd27f550316658a218c
jblasek <jacob.blasek1220@gmail.com>
2018-09-10 14:35:18 -0400

    what will the code do if you do not have a first of a last name?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
-
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbReader = new Scanner(System.in);
+        Scanner kbReaderstg = new Scanner(System.in);
+
+        System.out.println("What is your first name?");
+
+        String s = kbReader.next();
+
+        System.out.println("What is your last name?");
+
+        String s2 = kbReaderstg.next();
+
+        System.out.println("Your full name is " + s + " " + s2 + ".");
     }
 }
